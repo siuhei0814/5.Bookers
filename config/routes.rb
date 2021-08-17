67e852e29884cfe12303da50c4dd3b9f6ab738c0
siuhei0814 <thewindforest.0814@gmail.com>
2021-08-17 02:59:11 +0000

    202108171200_config

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root to: 'todolists#top'
-  get 'todolists/top'
-  get 'todolists/indexnew'
-  get 'todolists/show'
-  get 'todolists/edit'
+  get 'todolists' => 'todolists#index'
+  post 'todolists' => 'todolists#create'
+  get 'todolists/:id' => 'todolists#show',as:'todolist'
+  get 'todolists/:id/edit' => 'todolists#edit',as:'edit_todolist'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
