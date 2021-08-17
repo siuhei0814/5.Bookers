8920c555d167a09d2abc56c75fb633e1451a7281
siuhei0814 <thewindforest.0814@gmail.com>
2021-08-17 02:58:54 +0000

    202108171200_app

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -2,12 +2,31 @@ class TodolistsController < ApplicationController
   def top
   end
 
-  def indexnew
+  def index
+    @books = Book.all
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/todolists'
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  private
+
+  def book_params
+    params.permit(:title, :body)
   end
 end
