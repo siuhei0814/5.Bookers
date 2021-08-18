ad5d2d3afb75fe710d29f46baa8b081d7009c02f
siuhei0814 <thewindforest.0814@gmail.com>
2021-08-18 02:36:56 +0000

    202108181041_app

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -5,18 +5,15 @@ class TodolistsController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
   end
-
-# newアクションいらない？
-  # def new
-  #   @book = Book.new
-  # end
 
   def create
     @book = Book.new(book_params)
     if @book.save
       redirect_to '/todolists'
     else
+      @books = Book.all
       render :index
     end
   end
